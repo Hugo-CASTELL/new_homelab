681c7f20b8da2030c15ad349416abea4d42c40bb
Hugo CASTELL <100218176+Hugo-CASTELL@users.noreply.github.com>
2026-06-08 20:43:18 +0000

    Forgot to put mkdir in init.sh

--- infra/wyse/scripts/init.sh
+++ infra/wyse/scripts/init.sh
@@ -1,3 +1,5 @@
 # Secrets creation
+sudo mkdir -p /run/secrets/
+
 echo "Creation of Pi-hole Web Password: visit /run/secrets/pihole_web_password"
-echo "CHANGE_THIS" > /run/secrets/pihole_web_password
+sudo echo "CHANGE_THIS" > /run/secrets/pihole_web_password
