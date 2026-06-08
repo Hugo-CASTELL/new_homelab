d39c3089a4f64aaf0a6d02829ed2bcbca9a3978b
Hugo CASTELL <100218176+Hugo-CASTELL@users.noreply.github.com>
2026-06-08 20:45:27 +0000

    Permission denied

--- infra/wyse/scripts/init.sh
+++ infra/wyse/scripts/init.sh
@@ -2,4 +2,4 @@
 sudo mkdir -p /run/secrets/
 
 echo "Creation of Pi-hole Web Password: visit /run/secrets/pihole_web_password"
-sudo echo "CHANGE_THIS" > /run/secrets/pihole_web_password
+echo "CHANGE_THIS" | sudo tee /run/secrets/pihole_web_password > /dev/null
